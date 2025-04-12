ef15e86bba684acaeaaa40a14806570361aa996c
irkali <iraklishengelia94@gmail.com>
2025-04-12 12:07:24 +0400

    fix head

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <head>
+    <Head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Summit Sports Camps</title>
@@ -9,7 +9,7 @@
         href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap"
         rel="stylesheet"
       />
-    </head>
+    </Head>
     <body>
       <!-- Nav Bar -->
       <header class="w-full bg-black bg-opacity-80 py-4 px-6 flex items-center justify-between">
