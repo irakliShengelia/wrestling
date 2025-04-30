c304a05e3518c9e3187c3d828104d50c5a92f793
irkali <iraklishengelia94@gmail.com>
2025-04-30 22:06:06 +0400

    add google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/scripts'],
+  $production: {
+    scripts: {
+      registry: {
+        googleAnalytics: {
+          id: 'G-D04W0SQ23J',
+        }
+      }
+    }
+  }
 })
